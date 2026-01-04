954b5de1a71274c0e68066e73ec0b01e7f1e449b
GSWone <wganesh@gmail.com>
2026-01-03 23:34:36 -0800

    updates to index.rst

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Finance Foundations'
+copyright = '2026, G S Wone'
+author = 'G S Wone'
 
 release = '0.1'
 version = '0.1.0'
